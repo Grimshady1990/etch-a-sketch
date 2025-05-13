1d68613169b84c85f067e084e5798585264dae92
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-13 06:05:44 +0100

    Create a function the loops the creation of div squares
    The loop currently doesn't work as expected and will need tweaking

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,20 @@
 const container = document.createElement("div");
 document.body.appendChild(container);
+
+const gridCell = document.createElement("div");
+container.appendChild(gridCell);
+
+/*  ***TEST ELEMENT***
 const test = document.createElement("p");
 container.appendChild(test);
-test.textContent = "Dom test";
+test.textContent = "Dom test"; */
+
+function createGrid(root) {
+    let squareRoot = root ** 2;
+    for (i = 0; i <= squareRoot; i++){
+        gridCell = document.createElement("div");
+        container.appendChild(gridCell);
+        return;
+    }
+}
+createGrid(16);
