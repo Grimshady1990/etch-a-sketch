4918349a29e1090e6053ddf219f2eecbdff8cf8a
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-13 05:40:09 +0100

    Test if all files are linked

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+const container = document.createElement("div");
+document.body.appendChild(container);
+const test = document.createElement("p");
+container.appendChild(test);
+test.textContent = "Dom test";
