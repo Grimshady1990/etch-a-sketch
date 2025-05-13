ffef832370a9e3f00d23e550f90c451a84160785
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-13 21:08:30 +0100

    Complete the loops functionality
    Loop now creates a set amount of elements based on the input and finds the square.

--- assets/index.js
+++ assets/index.js
@@ -1,20 +1,26 @@
 const container = document.createElement("div");
 document.body.appendChild(container);
 
-const gridCell = document.createElement("div");
-container.appendChild(gridCell);
+
 
 /*  ***TEST ELEMENT***
 const test = document.createElement("p");
 container.appendChild(test);
 test.textContent = "Dom test"; */
 
+function gridCall() {
+    const gridCell = document.createElement("div");
+    gridCell.textContent = "test";
+    container.appendChild(gridCell);
+    
+
+}
+
 function createGrid(root) {
     let squareRoot = root ** 2;
-    for (i = 0; i <= squareRoot; i++){
-        gridCell = document.createElement("div");
-        container.appendChild(gridCell);
-        return;
-    }
+    for (i = 1; i <= squareRoot; i++){
+    gridCall();
+   
+    }   
 }
-createGrid(16);
+createGrid(3);
