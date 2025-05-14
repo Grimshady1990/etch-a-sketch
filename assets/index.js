83c2c05ab39fdd729d7ab9c11a7825d21d706a58
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-14 21:32:18 +0100

    Complete the first grid

--- assets/index.js
+++ assets/index.js
@@ -1,35 +1,32 @@
+const bodyStyle = document.querySelector("body");
+bodyStyle.setAttribute("style", "display: flex; flex-direction: column; align-items: center;");
 
 
 
+const container16 = document.createElement("div");
+container16.setAttribute("style", "display: flex; justify-content: center; width: 160px; height:160px; flex-wrap: wrap;");
+document.body.appendChild(container16);
+
+container16.addEventListener("mouseover", changeColor);
 
 
+function gridCall16() {
 
-
-
-/*  ***TEST ELEMENT***
-const test = document.createElement("p");
-container.appendChild(test);
-test.textContent = "Dom test"; */
-const container = document.createElement("div");
-container.setAttribute("style", "display: flex; flex-wrap: wrap;");
-document.body.appendChild(container);
-function gridCall() {
-    
-    
-    const gridCell = document.createElement("div");
-    
-    gridCell.setAttribute("style", "border: solid; border-width: 0.1px; background: green; width: 10px; height: 10px;");
-    
-    container.appendChild(gridCell);
-    
-
+    const gridCell16 = document.createElement("div");
+    gridCell16.setAttribute("style", "display: flex; flex-shrink: 1; background: green; width: 10px; height: 10px;");
+    container16.appendChild(gridCell16);
 }
 
-function createGrid(root) {
-    let squareRoot = root ** 2;
-    for (i = 1; i <= squareRoot; i++){
-    gridCall();
-   
-    }   
+function createGrid16() {
+    let squareRoot = 16 ** 2;
+    for (i = 1; i <= squareRoot; i++) {
+        gridCall16();
+    }
 }
-createGrid(16);
+createGrid16();
+
+function changeColor(event) {
+    console.log(event);
+    event.target.style.backgroundColor = "orange";
+}
+
